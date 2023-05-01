034f8ba6bdc4d5d952068642c188c7c54430c559
dimichai <dimichai@outlook.com>
2023-05-01 18:15:02 +0200

    pcn_amsterdam now uses PCNTNDP implementation

--- pcn_amsterdam.py
+++ pcn_amsterdam.py
@@ -5,7 +5,7 @@ import numpy as np
 from mo_gymnasium.utils import MORecordEpisodeStatistics
 import envs
 
-from morl_baselines.multi_policy.pcn.pcn import PCN
+from morl_baselines.multi_policy.pcn.pcn_tndp import PCNTNDP
 
 # todo add to config
 nr_stations = 20
@@ -25,9 +25,9 @@ def main():
 
     env = make_env()
 
-    agent = PCN(
+    agent = PCNTNDP(
         env,
-        scaling_factor=np.array([1, 1, 0.1, 0.1]),
+        scaling_factor=np.array([1, 1, 1, 1, 1, 0.1]),
         learning_rate=1e-3,
         batch_size=256,
         project_name="MORL-TNDP",
@@ -38,10 +38,11 @@ def main():
     agent.train(
         eval_env=make_env(),
         total_timesteps=int(1e7),
-        ref_point=np.array([0, 0, -200.0]),
+        ref_point=np.array([0, 0, 0, 0, 0]),
         num_er_episodes=20,
         max_buffer_size=50,
         num_model_updates=50,
+        starting_loc=(11, 15),
         # max_return=np.array([1.5, 1.5, -0.0]),
         # known_pareto_front=env.unwrapped.pareto_front(gamma=1.0),
     )
