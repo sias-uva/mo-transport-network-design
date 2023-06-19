ad455f5154ddd8f773026df581db38997c28e040
dimichai <9805570+dimichai@users.noreply.github.com>
2023-06-19 14:47:17 +0200

    Update hyperparam_search.py

--- hyperparam_search.py
+++ hyperparam_search.py
@@ -13,14 +13,19 @@ for batch_size in batch_sizes:
             %run train_pcn.py --env=dilemma --starting_loc_x=4 --starting_loc_y=0 --batch_size={batch_size} --lr={lr} --num_er_episodes={er_ep} --timesteps=2000
 
 #%% Amsterdam Hypeparameter Search
+# batch_sizes = [128, 256, 512]
+# lrs = [1e-1, 1e-2, 1e-3, 1e-4]
+# er_episodes = [25, 50, 100]
+# This is not a hyperparameter, it's the number of objectives
+
 batch_sizes = [128, 256, 512]
 lrs = [1e-1, 1e-2, 1e-3, 1e-4]
 er_episodes = [25, 50, 100]
-# This is not a hyperparameter, it's the number of objectives
+
 nr_groups = 5
 
 for batch_size in batch_sizes:
     for lr in lrs:
         for er_ep in er_episodes:
             print(f'batch_size: {batch_size}, lr: {lr}, er_episodes: {er_ep}')
-            %run train_pcn.py --env=amsterdam --starting_loc_x=9 --starting_loc_y=19 --batch_size={batch_size} --lr={lr} --nr_groups={nr_groups} --num_er_episodes={er_ep} --timesteps=10000
+            %run train_pcn.py --env=amsterdam --starting_loc_x=9 --starting_loc_y=19 --batch_size={batch_size} --lr={lr} --nr_groups={nr_groups} --num_er_episodes={er_ep} --timesteps=30000
