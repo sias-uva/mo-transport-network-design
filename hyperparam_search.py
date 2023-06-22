aa7dd204b1e489033e72b9ded619edce8e253618
Dimitris Michailidis <dmichai@ivi-h0.ivi.local>
2023-06-22 15:42:41 +0200

    hyperparam search updates

--- hyperparam_search.py
+++ hyperparam_search.py
@@ -1,10 +1,12 @@
 # It's a script that's not necessary to train and evaluate the PCN, but it's useful to perform grid-search on hyper-parameters.
 import argparse
 from pathlib import Path
+import random
 
 import numpy as np
 from train_pcn import main
 import time
+import torch
 
 ### Hyperparameters for Dilemma
 # batch_sizes = [128, 256]
@@ -16,13 +18,14 @@ import time
 ###
 
 # ### Hyperparameters for Amsterdam
-batch_sizes = [128, 256]
-lrs = [1e-1, 2e-1, 1e-2, 2e-2]
-er_episodes = [25, 50]
-max_buffer_sizes = [50]
-model_updates = [10, 30]
-hidden_dims = [32, 64, 128]
-timesteps = [10000]
+batch_sizes = [256, 512, 1024]
+lrs = [1e-1, 1e-2]
+er_episodes = [50, 100]
+max_buffer_sizes = [50, 100]
+model_updates = [5, 10]
+nr_layers = [3]
+hidden_dims = [64, 128]
+timesteps = [30000]
 # ###
 
 if __name__ == "__main__":
@@ -37,9 +40,14 @@ if __name__ == "__main__":
     # Episode horizon -- used as a proxy of both the budget and the number of stations (stations are not really costed)
     # parser.add_argument('--nr_stations', default=9, type=int)
     parser.add_argument('--num_policies', default=10, type=int)
+    parser.add_argument('--seed', default=42, type=int)
     parser.add_argument('--no_log', action='store_true', default=False)
 
     args = parser.parse_args()
+
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    random.seed(args.seed)
 
     # Some values are hardcoded for each environment (this is flexible, but we don't want to have to pass 100 arguments to the script)
     if args.env == 'dilemma':
@@ -47,6 +55,7 @@ if __name__ == "__main__":
         args.nr_stations = 9
         args.gym_env = 'motndp_dilemma-v0'
         args.groups_file = "groups.txt"
+        args.num_step_episodes = 10
         args.ignore_existing_lines = True
         args.experiment_name = "PCN-Dilemma"
         args.scaling_factor = np.array([1, 1, 0.1])
@@ -58,6 +67,7 @@ if __name__ == "__main__":
         args.nr_stations = 20
         args.gym_env = 'motndp_amsterdam-v0'
         args.groups_file = f"price_groups_{args.nr_groups}.txt"
+        args.num_step_episodes = 10
         args.ignore_existing_lines = True
         args.experiment_name = "PCN-Amsterdam"
         args.scaling_factor = np.array([1] * args.nr_groups + [0.01])
@@ -79,24 +89,26 @@ if __name__ == "__main__":
                 for max_buffer_size in max_buffer_sizes:
                     for model_update in model_updates:
                         for hidden_d in hidden_dims:
-                            for timestep in timesteps:
-                                counter += 1
-                                start_time = time.time()
-                                # Average running time of the last 5 runs
-                                avg_runtime = np.mean(running_times[-5:])
-                                print(f'Run {counter}/{total_runs} | Avg running time: {avg_runtime} | Estimated Time left: {(total_runs - counter)*avg_runtime / 60} minutes | env: {args.env} batch_size: {batch_size}, lr: {lr}, er_episodes: {er_ep}, max_buffer_size: {max_buffer_size}, model_update: {model_update}, hidden_size: {hidden_d}, timestep: {timestep}')
-                                args.batch_size = batch_size
-                                args.lr = lr
-                                args.num_er_episodes = er_ep
-                                args.max_buffer_size = max_buffer_size
-                                args.num_model_updates = model_update
-                                args.timesteps = timestep
-                                args.hidden_dim = hidden_d
+                            for nr_l in nr_layers:
+                                for timestep in timesteps:
+                                    counter += 1
+                                    start_time = time.time()
+                                    # Average running time of the last 5 runs
+                                    avg_runtime = np.mean(running_times[-5:])
+                                    print(f'Run {counter}/{total_runs} | Avg running time: {avg_runtime} | Estimated Time left: {(total_runs - counter)*avg_runtime / 60} minutes | env: {args.env} batch_size: {batch_size}, lr: {lr}, er_episodes: {er_ep}, max_buffer_size: {max_buffer_size}, model_update: {model_update}, hidden_size: {hidden_d}, timestep: {timestep}')
+                                    args.batch_size = batch_size
+                                    args.lr = lr
+                                    args.num_er_episodes = er_ep
+                                    args.max_buffer_size = max_buffer_size
+                                    args.num_model_updates = model_update
+                                    args.timesteps = timestep
+                                    args.hidden_dim = hidden_d
+                                    args.nr_layers = nr_l
 
-                                main(args)
+                                    main(args)
 
-                                execution_time = time.time() - start_time
-                                running_times.append(execution_time)
+                                    execution_time = time.time() - start_time
+                                    running_times.append(execution_time)
 
 #%% OLD CODE --  Dilemma Hypeparameter Search
 # batch_sizes = [128, 256, 512]
