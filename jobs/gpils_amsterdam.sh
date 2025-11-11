60924e100b7ef24f6966cb7f7814f75d0e00f0cf
Dimitris Michailidis <dimichai@outlook.com>
2025-11-11 11:21:05 +0100

    Add seed argument validation and update command scripts for LCN models

--- jobs/gpils_amsterdam.sh
+++ jobs/gpils_amsterdam.sh
@@ -4,12 +4,19 @@
 #SBATCH --job-name=gpils_ams
 #SBATCH --partition=all6000
 #SBATCH --account=all6000users
+##SBATCH --partition=all
 #SBATCH --gres=gpu:1
 #SBATCH --mem=62G
 #SBATCH --cpus-per-task=12
 #SBATCH --time=10:00:00
 
-SEED=6982
+if [ -z "$1" ]; then
+    echo "Error: Missing required argument <seed>"
+    echo "Usage: $0 <seed>"
+    exit 1
+fi
+
+SEED=$1
 
 # CMD="/home/dmichai/anaconda3/envs/mo-nw-design/bin/python train_gpipd.py --env amsterdam --starting_loc_x=9 --starting_loc_y=19 --nr_stations=10 --nr_groups=2 --total_timesteps=30000 --epsilon_decay_steps=20000 --timesteps_per_iter=5000 --nr_layers=3 --hidden_dim=128 --batch_size=16 --buffer_size=16384 --learning_rate=0.00001 --target_update_freq=20 --gradient_updates=2 --seed=$SEED"
 # CMD="/home/dmichai/anaconda3/envs/mo-nw-design/bin/python train_gpipd.py --env amsterdam --starting_loc_x=9 --starting_loc_y=19 --nr_stations=10 --nr_groups=3 --total_timesteps=30000 --epsilon_decay_steps=20000 --timesteps_per_iter=5000 --nr_layers=3 --hidden_dim=128 --batch_size=64 --buffer_size=256 --learning_rate=0.001 --target_update_freq=10 --gradient_updates=1 --seed=$SEED"
